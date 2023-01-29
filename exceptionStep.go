a08bd75a44d98e007248dd22356c3e98416030a9
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-29 16:37:42 +0800

    feat: add the new execute() method for the exeptionStep type
    this method saves the output of the command a bytes.Buffer{} and checks
    if it has anything in it, if it does, then it will return a step error
    with the said output
    
    will be useful for the gofmt usage for pipeline step 3 but also for any
    another step that shouldn't return an output if everything was correct

--- exceptionStep.go
+++ exceptionStep.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
 // exceptionStep is a step type that has a different execute() method logic
 // in specific cases where the output of the actual executing command's output
 // needs to be handled, like the gofmt
@@ -12,4 +18,33 @@ func newExceptionStep(name, exe, message, proj string, args []string) exceptionS
 
 	s.step = newStep(name, exe, message, proj, args)
 	return s
+}
+
+func (s exceptionStep) execute() (string, error) {
+	cmd := exec.Command(s.exe, s.args...)
+
+	w := &bytes.Buffer{}
+	cmd.Stdout = w
+
+	cmd.Dir = s.proj
+
+	if err := cmd.Run(); err != nil {
+		return "", &stepErr{
+			step:  s.name,
+			msg:   "failed to execute",
+			cause: err,
+		}
+	}
+
+	// if len of bytes in stdout is more than 0 then print the
+	// message from stdout
+	if w.Len() > 0 {
+		return "", &stepErr{
+			step:  s.name,
+			msg:   fmt.Sprintf("invalid format: %s", w.String()),
+			cause: nil,
+		}
+	}
+
+	return s.message, nil
 }
