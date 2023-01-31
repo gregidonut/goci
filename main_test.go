1d3690e5c21a66490daca906dc0c7b0c70b5ec22
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-31 16:17:29 +0800

    test: add test helper that will setup a git server for testing git step
    this test helper is responsible for setting up the git repo in the
    project directory and also the git server in a temporary directory,
    every prerequisite that the project presumably has already done before
    running 'git push' which is the current final step of the goci tool

--- main_test.go
+++ main_test.go
@@ -3,7 +3,11 @@ package main
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"github.com/gregidonut/goci/step"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 )
 
@@ -61,5 +65,71 @@ Git Push: SUCCESS
 				t.Errorf("want: %q, got %q", tt.wantOut, want)
 			}
 		})
+	}
+}
+
+// setupGit is responsible for setting up the git repo in the
+// project directory and also the git server in a temporary directory,
+// every prerequisite that the project presumably has already done before
+// running 'git push'
+func setupGit(t *testing.T, proj string) func() {
+	t.Helper()
+
+	gitExec, err := exec.LookPath("git")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tempDir, err := os.MkdirTemp("", "gocitest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	projPath, err := filepath.Abs(proj)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	remoteURI := fmt.Sprintf("file://%s", tempDir)
+
+	gitCmdList := []struct {
+		args []string
+		dir  string
+		env  []string
+	}{
+		// initialize remote repository in tempDir
+		{args: []string{"init", "--bare"}, dir: tempDir, env: nil},
+		// initialize git repository on projPath
+		{args: []string{"init"}, dir: projPath, env: nil},
+		// register remote repository(in tempDir) as remoteURI in projPath's git repo
+		{args: []string{"remote", "add", "origin", remoteURI}, dir: projPath, env: nil},
+		// add files in projPath to projPath's git repo's index
+		{args: []string{"add", "."}, dir: projPath, env: nil},
+		// commit staged in projPath repo with following environment variables
+		{args: []string{"commit", "-m", "test"}, dir: projPath,
+			env: []string{
+				"GIT_COMMITTER_NAME=test",
+				"GIT_COMMITTER_EMAIL=test@example.com",
+				"GIT_AUTHOR_NAME=test",
+				"GIT_AUTHOR_EMAIL=test@example.com",
+			}},
+	}
+
+	for _, g := range gitCmdList {
+		gitCmd := exec.Command(gitExec, g.args...)
+		gitCmd.Dir = g.dir
+
+		if g.env != nil {
+			gitCmd.Env = append(os.Environ(), g.env...)
+		}
+
+		if err := gitCmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	return func() {
+		os.RemoveAll(tempDir)
+		os.RemoveAll(filepath.Join(projPath, ".git"))
 	}
 }
